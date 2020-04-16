dd68641199570670d0ffe237d1564a6798e4ea32
valdevweb <vmontusclat@gmail.com>
2020-04-16 12:02:15 +0200

    reajout cdn boostrap

--- view/_head.php
+++ view/_head.php
@@ -6,18 +6,17 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="icon" href="<?= (basename($_SERVER['PHP_SELF'])=='index.php')? 'wtg.ico': '../wtg.ico'?>" />
-	
 	<script src="<?= (basename($_SERVER['PHP_SELF'])=='index.php')? 'vendor/jquery.js':'../vendor/jquery.js'?> "></script>
-
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
 	<link rel="stylesheet" href="<?= (basename($_SERVER['PHP_SELF'])=='index.php') ? 'css/main.css' :'../css/main.css'?> ">	
-		<!-- style de la page -->
+	<!-- style de la page -->
 	<?php
 	if(isset($cssFile)){
-	
+		
 		$hexplodedCssfile=explode('/',$cssFile);
 		$hcssFileName=$hexplodedCssfile[count($hexplodedCssfile)-1];
 		if(basename($_SERVER['PHP_SELF'])=='index.php'){
