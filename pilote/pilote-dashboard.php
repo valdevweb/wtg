1ec4542830fdce3d1f9d0e72333bb926eb17b46f
valdevweb <vmontusclat@gmail.com>
2020-04-16 11:41:19 +0200

    creat

---
+++ pilote/pilote-dashboard.php
@@ -0,0 +1,66 @@
+
+<?php
+require('../config/autoload.php');
+
+// if(!isset($_SESSION['id'])){
+// 	echo "pas de variable session";
+// 	header('Location:'. DIR_NOSESSION);
+// }
+//------------------------------------------------------
+//			css dynamique
+//----------------------------------------------------------------
+$pageCss=explode(".php",basename(__file__));
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."css/".$pageCss.".css";
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------
+//			FONCTION
+//------------------------------------------------------
+
+ //------------------------------------------------------
+//			DECLARATIONS
+//------------------------------------------------------
+$errors=[];
+$success=[];
+//------------------------------------------------------
+//			VIEW
+//------------------------------------------------------
+include('../view/_head.php');
+
+?>
+<!--********************************
+DEBUT CONTENU CONTAINER
+*********************************-->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col pt-3 pl-5 mb-3 text-center">
+			<h1 class="underline-anim">Pilote dahsboard</h1>			
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col">
+			<?php
+			include('../view/_errors.php');
+				echo "<pre>";
+				print_r($_SESSION);
+				echo '</pre>';
+				
+			?>
+		</div>
+	</div>
+
+	<!-- ./container -->
+</div>
+
+<?php
+require '../view/_footer.php';
+?>
